75f23ec8b5e9d8a27165353bd9a3c495f3355020
Deiadara <nikos@commonprefix.com>
2025-09-15 18:23:56 +0300

    test(parser_message_approved): add unit tests

--- src/transaction_parser/parser_message_approved.rs
+++ src/transaction_parser/parser_message_approved.rs
@@ -156,154 +156,98 @@ impl Parser for ParserMessageApproved {
     }
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//     use solana_transaction_status::UiCompiledInstruction;
+#[cfg(test)]
+mod tests {
+    use solana_sdk::signature::Signature;
+    use solana_transaction_status::UiInstruction;
 
-//     #[tokio::test]
-//     async fn test_decode_queue_entry_and_parse_gas_credit() {
-//         // Build an AddedGas instruction: [CPI_DISC(8)] [ADDED_DISC(8)] [config_pda(32)] [tx_hash(64)] [log_index(8)] [refund_address(32)] [gas_fee_amount(8)]
-//         let mut data: Vec<u8> = Vec::new();
-//         data.extend_from_slice(&CPI_EVENT_DISC);
-//         data.extend_from_slice(&NATIVE_GAS_ADDED_EVENT_DISC);
-//         // config_pda
-//         data.extend_from_slice(&[0x11; 32]);
-//         // tx_hash (64 bytes)
-//         data.extend_from_slice(&[0x22; 64]);
-//         // log_index
-//         data.extend_from_slice(&(5u64).to_le_bytes());
-//         // refund_address (dummy pubkey bytes)
-//         data.extend_from_slice(&[0x33; 32]);
-//         // gas_fee_amount
-//         data.extend_from_slice(&(1000u64).to_le_bytes());
-//         let ci = UiCompiledInstruction {
-//             program_id_index: 4,
-//             accounts: vec![3],
-//             data: bs58::encode(data).into_string(),
-//             stack_height: Some(2),
-//         };
+    use super::*;
+    use crate::test_utils::fixtures::transaction_fixtures;
+    use crate::transaction_parser::parser_message_approved::ParserMessageApproved;
+    #[tokio::test]
+    async fn test_parser() {
+        let txs = transaction_fixtures();
 
-//         let mut parser = ParserNativeGasAdded::new("dummy_sig".to_string(), ci)
-//             .await
-//             .unwrap();
+        let tx = txs[1].clone();
+        let compiled_ix: UiCompiledInstruction = match tx.ixs[0].instructions[1].clone() {
+            UiInstruction::Compiled(ix) => ix,
+            _ => panic!("expected a compiled instruction"),
+        };
 
-//         assert!(
-//             parser.is_match().await.unwrap(),
-//             "parser should match gas credit"
-//         );
-//         assert!(parser.parse().await.unwrap(), "parser should parse message");
+        let mut parser = ParserMessageApproved::new(tx.signature.to_string(), compiled_ix)
+            .await
+            .unwrap();
+        assert!(parser.is_match().await.unwrap());
+        let sig = Signature::from([
+            40, 230, 73, 108, 137, 197, 160, 39, 204, 198, 21, 197, 67, 88, 19, 21, 106, 217, 129,
+            223, 19, 151, 242, 199, 144, 241, 23, 113, 90, 15, 12, 205, 134, 111, 95, 166, 31, 93,
+            42, 71, 167, 183, 101, 94, 161, 165, 85, 205, 76, 215, 192, 176, 200, 225, 48, 206,
+            147, 33, 28, 240, 150, 167, 28, 117,
+        ])
+        .to_string();
+        parser.parse().await.unwrap();
+        let event = parser.event(Some(format!("{}-1", sig))).await.unwrap();
+        match event {
+            Event::MessageApproved { ref common, .. } => {
+                let expected_event = Event::MessageApproved {
+                    common: CommonEventFields {
+                        r#type: "MESSAGE_APPROVED".to_owned(),
+                        event_id: sig.clone(),
+                        meta: Some(MessageApprovedEventMetadata {
+                            common_meta: EventMetadata {
+                                tx_id: Some(sig.clone()),
+                                from_address: None,
+                                finalized: None,
+                                source_context: None,
+                                timestamp: common
+                                    .meta
+                                    .as_ref()
+                                    .unwrap()
+                                    .common_meta
+                                    .timestamp
+                                    .clone(),
+                            },
+                            command_id: Some(
+                                encode(parser.parsed.as_ref().unwrap().command_id).into_string(),
+                            ),
+                        }),
+                    },
+                    message: GatewayV2Message {
+                        message_id: parser.parsed.as_ref().unwrap().message_id.clone(),
+                        source_chain: parser.parsed.as_ref().unwrap().source_chain.clone(),
+                        source_address: parser.parsed.as_ref().unwrap().source_address.clone(),
+                        destination_address: parser
+                            .parsed
+                            .as_ref()
+                            .unwrap()
+                            .destination_address
+                            .to_string(),
+                        payload_hash: hex::encode(parser.parsed.as_ref().unwrap().payload_hash),
+                    },
+                    cost: Amount {
+                        token_id: None,
+                        amount: "0".to_string(),
+                    },
+                };
+                assert_eq!(event, expected_event);
+            }
+            _ => panic!("Expected MessageApproved event"),
+        }
+    }
 
-//         let event = parser
-//             .event(Some("foo".to_string()))
-//             .await
-//             .expect("event should be produced");
-//         println!("{:?}", event);
-//         match event {
-//             Event::GasCredit {
-//                 message_id,
-//                 refund_address,
-//                 payment,
-//                 ..
-//             } => {
-//                 assert_eq!(message_id, "foo");
-//                 assert!(!refund_address.is_empty());
-//                 assert!(!payment.amount.is_empty());
-//             }
-//             other => panic!("Expected GasCredit, got {:?}", other),
-//         }
-//     }
+    #[tokio::test]
+    async fn test_no_match() {
+        let txs = transaction_fixtures();
 
-//     #[tokio::test]
-//     async fn test_parser_native_gas_added() {
-//         // Reuse the synthetic instruction builder above
-//         let mut data: Vec<u8> = Vec::new();
-//         data.extend_from_slice(&CPI_EVENT_DISC);
-//         data.extend_from_slice(&NATIVE_GAS_ADDED_EVENT_DISC);
-//         data.extend_from_slice(&[0xAA; 32]);
-//         data.extend_from_slice(&[0xBB; 64]);
-//         data.extend_from_slice(&(7u64).to_le_bytes());
-//         data.extend_from_slice(&[0xCC; 32]);
-//         data.extend_from_slice(&(4242u64).to_le_bytes());
-//         let ci = UiCompiledInstruction {
-//             program_id_index: 4,
-//             accounts: vec![3],
-//             data: bs58::encode(data).into_string(),
-//             stack_height: Some(2),
-//         };
+        let tx = txs[0].clone();
+        let compiled_ix: UiCompiledInstruction = match tx.ixs[0].instructions[0].clone() {
+            UiInstruction::Compiled(ix) => ix,
+            _ => panic!("expected a compiled instruction"),
+        };
+        let parser = ParserMessageApproved::new(tx.signature.to_string(), compiled_ix)
+            .await
+            .unwrap();
 
-//         let mut parser = ParserNativeGasAdded::new("dummy_sig_2".to_string(), ci)
-//             .await
-//             .unwrap();
-//         assert!(parser.is_match().await.unwrap());
-//         parser.parse().await.unwrap();
-//         let ev = parser.event(Some("bar".to_string())).await.unwrap();
-//         match ev {
-//             Event::GasCredit {
-//                 message_id,
-//                 payment,
-//                 ..
-//             } => {
-//                 assert_eq!(message_id, "bar");
-//                 assert_eq!(payment.amount, "4242");
-//             }
-//             _ => panic!("Expected GasCredit"),
-//         }
-//     }
-
-// #[tokio::test]
-// async fn test_parser_gas_paid() {
-//     let solana_transactions = fixture_native_gas_paid();
-
-//     let tx = solana_transactions[0].clone();
-//     let ix = tx.ixs[0].instructions[0].clone();
-//     let UiInstruction::Compiled(ci) = ix else {
-//         panic!("expected compiled instruction")
-//     };
-//     let mut parser = ParserNativeGasPaid::new(tx.signature.to_string(), ci)
-//         .await
-//         .unwrap();
-
-//     assert!(parser.is_match().await.unwrap());
-//     assert!(parser.message_id().await.is_ok());
-//     parser.parse().await.unwrap();
-//     let event = parser.event(Some("foo".to_string())).await.unwrap();
-//     match event {
-//         Event::GasCredit {
-//             common,
-//             message_id,
-//             refund_address,
-//             payment,
-//         } => {
-//             assert_eq!(message_id, "foo");
-//             assert_eq!(
-//                 refund_address,
-//                 "0:e1e633eb701b118b44297716cee7069ee847b56db88c497efea681ed14b2d2c7"
-//             );
-//             assert_eq!(payment.amount, "198639200");
-
-//             let meta = &common.meta.as_ref().unwrap();
-//             assert_eq!(
-//                 meta.tx_id.as_deref(),
-//                 Some("PzeZlujUPePAMw0Fz/eYeCRz11/X/f5YzUjfYXomzS8=")
-//             );
-//         }
-//         _ => panic!("Expected GasCredit event"),
-//     }
-// }
-
-// #[tokio::test]
-// async fn test_no_match() {
-//     let solana_transactions = fixture_native_gas_paid();
-
-//     let tx = solana_transactions[0].clone();
-//     let ix = tx.ixs[0].instructions[0].clone();
-//     let UiInstruction::Compiled(ci) = ix else {
-//         panic!("expected compiled instruction")
-//     };
-//     let parser = ParserNativeGasPaid::new(tx.signature.to_string(), ci)
-//         .await
-//         .unwrap();
-//     assert!(!parser.is_match().await.unwrap());
-// }
-//}
+        assert!(!parser.is_match().await.unwrap());
+    }
+}
