563039f11bd6cef2c499ebc314baef4c8da22a41
Deiadara <nikos@commonprefix.com>
2025-09-16 15:10:28 +0300

    feat(parser_its): add parser_its_interchain_transfer

---
+++ src/transaction_parser/parser_its_interchain_transfer.rs
@@ -0,0 +1,153 @@
+use std::collections::HashMap;
+
+use crate::error::TransactionParsingError;
+use crate::transaction_parser::discriminators::{
+    CPI_EVENT_DISC, ITS_INTERCHAIN_TRANSFER_EVENT_DISC,
+};
+use crate::transaction_parser::message_matching_key::MessageMatchingKey;
+use crate::transaction_parser::parser::{Parser, ParserConfig};
+use async_trait::async_trait;
+use borsh::BorshDeserialize;
+use relayer_core::gmp_api::gmp_types::{Amount, CommonEventFields, Event, EventMetadata};
+use solana_sdk::pubkey::Pubkey;
+use solana_transaction_status::UiCompiledInstruction;
+use tracing::{debug, warn};
+
+#[derive(BorshDeserialize, Clone, Debug)]
+pub struct InterchainTransfer {
+    pub token_id: [u8; 32],
+    pub source_address: Pubkey,
+    pub source_token_account: Pubkey,
+    pub destination_chain: String,
+    pub destination_address: Vec<u8>,
+    pub amount: u64,
+    pub data_hash: [u8; 32],
+}
+
+pub struct ParserInterchainTransfer {
+    signature: String,
+    parsed: Option<InterchainTransfer>,
+    instruction: UiCompiledInstruction,
+    config: ParserConfig,
+}
+
+impl ParserInterchainTransfer {
+    pub(crate) async fn new(
+        signature: String,
+        instruction: UiCompiledInstruction,
+    ) -> Result<Self, TransactionParsingError> {
+        Ok(Self {
+            signature,
+            parsed: None,
+            instruction,
+            config: ParserConfig {
+                event_cpi_discriminator: CPI_EVENT_DISC,
+                event_type_discriminator: ITS_INTERCHAIN_TRANSFER_EVENT_DISC,
+            },
+        })
+    }
+
+    fn try_extract_with_config(
+        instruction: &UiCompiledInstruction,
+        config: ParserConfig,
+    ) -> Option<InterchainTransfer> {
+        let bytes = match bs58::decode(&instruction.data).into_vec() {
+            Ok(bytes) => bytes,
+            Err(e) => {
+                warn!("failed to decode bytes: {:?}", e);
+                return None;
+            }
+        };
+        if bytes.len() < 16 {
+            return None;
+        }
+
+        if bytes.get(0..8) != Some(&config.event_cpi_discriminator) {
+            debug!(
+                "expected event cpi discriminator, got {:?}",
+                bytes.get(0..8)
+            );
+            return None;
+        }
+        if bytes.get(8..16) != Some(&config.event_type_discriminator) {
+            debug!(
+                "expected event type discriminator, got {:?}",
+                bytes.get(8..16)
+            );
+            return None;
+        }
+
+        let payload = bytes.get(16..)?;
+        match InterchainTransfer::try_from_slice(payload) {
+            Ok(event) => {
+                debug!("Interchain Transfer event={:?}", event);
+                Some(event)
+            }
+            Err(_) => None,
+        }
+    }
+}
+
+#[async_trait]
+impl Parser for ParserInterchainTransfer {
+    async fn parse(&mut self) -> Result<bool, TransactionParsingError> {
+        if self.parsed.is_none() {
+            self.parsed = Self::try_extract_with_config(&self.instruction, self.config);
+        }
+        Ok(self.parsed.is_some())
+    }
+
+    async fn is_match(&mut self) -> Result<bool, TransactionParsingError> {
+        match Self::try_extract_with_config(&self.instruction, self.config) {
+            Some(parsed) => {
+                self.parsed = Some(parsed);
+                Ok(true)
+            }
+            None => Ok(false),
+        }
+    }
+
+    async fn key(&self) -> Result<MessageMatchingKey, TransactionParsingError> {
+        Err(TransactionParsingError::Message(
+            "MessageMatchingKey is not available for InterchainTransfer".to_string(),
+        ))
+    }
+
+    async fn event(&self, _: Option<String>) -> Result<Event, TransactionParsingError> {
+        let parsed = self
+            .parsed
+            .clone()
+            .ok_or_else(|| TransactionParsingError::Message("Missing parsed".to_string()))?;
+
+        Ok(Event::ITSInterchainTransfer {
+            common: CommonEventFields {
+                r#type: "ITS/INTERCHAIN_TRANSFER".to_owned(),
+                event_id: format!("{}-its-interchain-transfer", self.signature.clone()),
+                meta: Some(EventMetadata {
+                    tx_id: Some(self.signature.clone()),
+                    from_address: None,
+                    finalized: None,
+                    source_context: Some(HashMap::from([(
+                        "source_token_account".to_owned(),
+                        parsed.source_token_account.to_string(),
+                    )])),
+                    timestamp: chrono::Utc::now()
+                        .to_rfc3339_opts(chrono::SecondsFormat::Secs, true),
+                }),
+            },
+            source_address: parsed.source_address.to_string(),
+            destination_chain: parsed.destination_chain.clone(),
+            destination_address: hex::encode(parsed.destination_address),
+            data_hash: hex::encode(parsed.data_hash),
+            message_id: self.signature.clone(),
+            token_spent: Amount {
+                token_id: Some(hex::encode(parsed.token_id)),
+                amount: parsed.amount.to_string(),
+            },
+        })
+    }
+
+    async fn message_id(&self) -> Result<Option<String>, TransactionParsingError> {
+        Ok(None)
+    }
+}
