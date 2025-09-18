ea569431019ad658c9f3af37ff3928ee6e7e2da5
Deiadara <nikos@commonprefix.com>
2025-09-18 13:32:09 +0300

    chore(discriminators): add comment

--- src/transaction_parser/discriminators.rs
+++ src/transaction_parser/discriminators.rs
@@ -1,5 +1,6 @@
 pub const CPI_EVENT_DISC: [u8; 8] = [228, 69, 165, 46, 81, 203, 154, 29]; // 0xe445a52e51cb9a1d (EVENT_IX_TAG in LE)
 
+// gateway and gas_service events
 pub const NATIVE_GAS_PAID_EVENT_DISC: [u8; 8] = [232, 125, 221, 19, 212, 213, 137, 199];
 pub const NATIVE_GAS_ADDED_EVENT_DISC: [u8; 8] = [163, 149, 4, 134, 245, 100, 202, 54];
 pub const NATIVE_GAS_REFUNDED_EVENT_DISC: [u8; 8] = [43, 222, 83, 117, 24, 237, 201, 202];
