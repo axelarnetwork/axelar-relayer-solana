770e21b00a12fb9e6c2ce4d850583fa9e9e67c6f
Deiadara <nikos@commonprefix.com>
2025-12-24 16:29:19 +0100

    chore(includer): report error even if getting tx cost fails

--- src/includer.rs
+++ src/includer.rs
@@ -352,7 +352,7 @@ impl<
                         .client
                         .get_transaction_cost_from_signature(&signature)
                         .await
-                        .map_err(|e| IncluderError::GenericError(e.to_string()))?;
+                        .unwrap_or(None); // if we fail to get the cost we still need to report the GMP
 
                     // Include ALT cost if ALT transaction was sent successfully
                     let total_reverted_cost = reverted_tx_cost
